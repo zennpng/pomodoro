902d2f96f9b60ff765b2f350eb6a9c1c01447488
zennpng <48861561+zennpng@users.noreply.github.com>
2020-07-15 14:30:27 +0800

    add component import

--- App.js
+++ App.js
@@ -1,88 +1,14 @@
 import React from 'react';
 import {View, Button, Text, StyleSheet, Vibration} from 'react-native'
+import Counter from './counter'
 
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  work: {
-    backgroundColor: 'lightgreen',
-  },
-  rest: {
-    backgroundColor: 'yellow',
-  },
-  count: {
-    fontSize: 25,
-    textAlign:'center',
-  },
-  text :{
-    fontSize: 40,
   }
 })
-
-
-class Counter extends React.Component {
-    constructor(props) {
-    super(props)
-    this.state = {
-      count: 1500,
-      work: true,
-    }
-  }
-  
-  componentDidMount() {
-      this.interval = setInterval(this.dec, 1000)
-  }
-
-  componentWillUnmount() { 
-    clearInterval(this.interval)
-  }
-  
-  dec = () => {
-    this.setState(prevState => ({
-      count: prevState.count -1,
-    }))
-  }
-
-  render() {
-    if (this.state.count < 0 && this.state.work === true) {
-      this.setState({count:300, work:false})
-      Vibration.vibrate()
-      return (
-        <View style={styles.rest}>
-        <Text style={styles.text}> Rest Time! </Text>
-        <Text style={styles.count}> {this.state.count} seconds left </Text> 
-        </View>
-      )
-    }else if (this.state.count < 0 && this.state.work === false) {
-      this.setState({count:1500, work:true})
-      Vibration.vibrate()
-      return (
-        <View style={styles.work}>
-        <Text style={styles.text}> Work Time! </Text>
-        <Text style={styles.count}> {this.state.count} seconds left </Text> 
-        </View>
-      )
-    }else if (this.state.count >= 0 && this.state.work === true) {
-    return (
-      <View style={styles.work}>
-      <Text style={styles.text}> Work Time! </Text>
-      <Text style={styles.count}> {this.state.count} seconds left </Text> 
-      </View>
-    )
-    }else{
-      return (
-        <View style={styles.rest}>
-        <Text style={styles.text}> Rest Time! </Text>
-        <Text style={styles.count}> {this.state.count} seconds left </Text> 
-        </View>
-      )
-    }
-  }
-}
-
 
 export default class App extends React.Component {
   constructor(props) {
@@ -91,15 +17,10 @@ export default class App extends React.Component {
       showCounter: false,
     }
   }
-
-  workrest = () => this.setState(prevState => ({
-    work: !prevState.work,
-  }))
   
   toggleCounter = () => this.setState(prevState => ({
     showCounter: !prevState.showCounter,
   }))
-
 
   render() {
     if (this.state.showCounter) {
